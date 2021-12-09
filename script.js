cae8c0304fcf8ea5a08c3cf859e8652b5f65aac0
Nick <nickax@gmail.com>
2021-12-09 08:48:08 +0000

    Basic functionality

--- script.js
+++ script.js
@@ -1,39 +1,87 @@
 "use strict"
 
-let makes={ford:[],tesla:[],bmw:[],vauxhall:[]}
-makes.ford="Fiesta,Focus,KA,Mondeo,Fusion,Transit".split(",")
-makes.tesla="3,sport,Cybertruck".split(",")
-makes.bmw="3,5,i3,i7,1 series".split(",")
-makes.vauxhall="Corsa,Insignia,Movano,Astra,Senator"
+let makes={Ford:[],Tesla:[],BMW:[],Vauxhall:[]}
+makes.Ford="Fiesta,Focus,KA,Mondeo,Fusion,B-Max".split(",")
+makes.Tesla="3,Sport,Cybertruck".split(",")
+makes.BMW="3,5,i3,i7,1 Series".split(",")
+makes.Vauxhall="Corsa,Insignia,Movano,Astra,Senator".split(",")
+makes.Toyota="Yaris,Celica,MR2,Avensis,Rav4".split(",")
+makes.Nissan="Leaf,Primera,Juke,Micra".split(",")
 
-let cars=generateRandomCars(makes,3)
 
+let cars=generateRandomCars(makes,100)
 
 let holder = document.getElementById("holder")
 
-for (let i=0;i<cars.length;i++){
-    let card = document.createElement("div")
-    card.classList.add("card")        
-    holder.appendChild(card)
+let saveButton=document.getElementById("save")
+saveButton.addEventListener("click", addCar )
 
-    let heading= document.createElement("h1")
-    heading.innerHTML= cars[i].make + " " + cars[i].model
-    card.appendChild(heading)
+renderCars()
 
-    let price= document.createElement("p")
-    price.innerHTML= cars[i].price
-    card.appendChild(price)
-    
+function renderCars(){
+
+    holder.innerHTML=''
+    for (let i=0;i<cars.length;i++){
+        let card = document.createElement("div")
+        card.classList.add("card")        
+        holder.appendChild(card)
+
+        let heading= document.createElement("h1")
+        heading.innerHTML= cars[i].make + " " + cars[i].model
+        card.appendChild(heading)
+
+        let picture=document.createElement("img")
+        picture.src= cars[i].picture
+        card.appendChild(picture)
+        picture.style.width="80%"
+
+        let price= document.createElement("p")
+        price.innerHTML= "£" + cars[i].price
+        card.appendChild(price) 
+        
+        let mileage= document.createElement("p")
+        mileage.innerHTML= cars[i].mileage + " miles"
+        card.appendChild(mileage) 
+        
+        let color=document.createElement("div")
+        color.classList.add("colorSquare")
+        card.appendChild(color)
+        color.style.backgroundColor=cars[i].colour
+    }
 }
 
+function addCar(){
+
+
+    let make=document.getElementById("make").value
+    let model=document.getElementById("model").value
+    let price=document.getElementById("price").value
+    let color=document.getElementById("color").value
+
+    let car ={make:make,model:model,price:price}
+    cars.push(car)      
+
+    renderCars()
+       
+
+}
 
 function generateRandomCars(makes,numCars){
+
+    let colours="red,orange,yellow,green,blue,violet,black,white,gray".split(",")
+    let pics=[]
+    pics.push("https://cdn.tradecentregroup.io/image/upload/q_auto/f_auto/w_400/web/Group/cars/seat/ibiza.png")
+    pics.push("https://cdn.tradecentregroup.io/image/upload/q_auto/f_auto/w_400/web/Group/cars/peugeot/108.png")
+    pics.push("https://cdn.tradecentregroup.io/image/upload/q_auto/f_auto/w_400/web/Group/cars/renault/clio.png")
+    pics.push("https://cdn.tradecentregroup.io/image/upload/q_auto/f_auto/w_400/web/Group/cars/ford/b-max.png")
+    pics.push("https://cdn.tradecentregroup.io/image/upload/q_auto/f_auto/w_400/web/Group/cars/bmw/1-series.png")
+    pics.push("https://cdn.tradecentregroup.io/image/upload/q_auto/f_auto/w_400/web/Group/cars/nissan/qashqai.png")
 
     let cars=[]
     for(let i=0;i<numCars;i++){
         let make=pickFrom(Object.keys(makes))  //Pick a manufacturer from the makes object
         let model = pickFrom(makes[make])
-        cars.push({make:make,model:model,price:randomInteger(10000),mileage:randomInteger(100000)})    
+        cars.push({make:make,model:model,price:randomInteger(1000)*10,mileage:randomInteger(200000),colour:pickFrom(colours),picture:pickFrom(pics)})    
     }
 
     return cars  //send back the 'complete' list of cars
